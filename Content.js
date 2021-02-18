9ebcc99e94b34d35ec2ce4df23b29294e76e0b08
fabriken <techtux@gmail.com>
2021-02-18 14:43:59 +0930

    Update Content.js
    solved for 38 instead of 37 nodes

--- Content.js
+++ Content.js
@@ -11,10 +11,12 @@ docReady(function() {
 
   console.log('------QLink 1.7------');
   let tb = document.getElementsByTagName("tbody");
+ // let tb = document.getElementsByTagName("td");
+// console.log(tb);
 
   // ---------------- Check for Potential match ----------------
 
-  let potential = tb[1].childNodes[8].lastElementChild.firstElementChild;
+  let potential = tb[1].childNodes[8].lastElementChild.firstElementChild.color;
 
   // ---------------- Find account number ----------------
 
@@ -22,18 +24,18 @@ docReady(function() {
 
   // ---------------- Find group ---------------
 
-  let g = tb[1].childNodes[8].innerHTML;
-  let group_start = g.indexOf('Group:') + 7;
-  let group_end = g.indexOf('(',group_start);
-  let group_get = g.substring(group_start,group_end);
+  let gr = tb[1].childNodes[8].innerHTML;
+  let group_start = gr.indexOf('Group:') + 7;
+  let group_end = gr.indexOf('(',group_start);
+  let group_get = gr.substring(group_start,group_end);
   let group = group_get.trim();
 
   // ---------------- find CustID ----------------
 
-  let c = tb[1].childNodes[2].childNodes[2].innerHTML;
-  let cust_start = c.indexOf('CustId') + 1;
-  let cust_end = c.indexOf('"',cust_start);
-  let cust_get = c.substring(cust_start,cust_end);
+  let cu = tb[1].childNodes[2].childNodes[2].innerHTML;
+  let cust_start = cu.indexOf('CustId') + 1;
+  let cust_end = cu.indexOf('"',cust_start);
+  let cust_get = cu.substring(cust_start,cust_end);
   let cust = cust_get.replace(/[^0-9]+/g,"");
   let custID = cust;
 
@@ -45,14 +47,55 @@ docReady(function() {
   
   let currstat = tb[0].childNodes[10].defaultValue;
 
-  let ContactOK = tb[7].childNodes[8].childNodes[3].childNodes[3].value;
-  let PtnrContactOK = tb[7].childNodes[8].childNodes[3].childNodes[7].value;
-  let ContactOffers = tb[7].childNodes[10].childNodes[3].childNodes[3].value;
-  let ContactNewsltr = tb[7].childNodes[10].childNodes[3].childNodes[7].value;
-  let GoodEmail = tb[9].childNodes[0].lastElementChild.lastElementChild.value;
-  let GoodMobile = tb[9].childNodes[2].lastElementChild.lastElementChild.value;
-  let GoodAddr = tb[9].childNodes[4].lastElementChild.lastElementChild.value;
-  let contact = tb[8].lastElementChild.value;
+  if (tb.length == 37) {
+    var a = tb[7].childNodes[8].childNodes[3].childNodes[3].value;
+  } else {
+    var a = tb[8].childNodes[8].childNodes[3].childNodes[3].value;
+  }
+  if (tb.length == 37) {
+    var b = tb[7].childNodes[8].childNodes[3].childNodes[7].value;
+  } else {
+    var b = tb[8].childNodes[8].childNodes[3].childNodes[7].value;
+  }
+  if (tb.length == 37) {
+    var c = tb[7].childNodes[10].childNodes[3].childNodes[3].value;
+  } else {
+    var c = tb[8].childNodes[10].childNodes[3].childNodes[3].value;
+  }
+  if (tb.length == 37) {
+    var d = tb[7].childNodes[10].childNodes[3].childNodes[7].value;
+  } else {
+    var d = tb[8].childNodes[10].childNodes[3].childNodes[7].value;
+  }
+  if (tb.length == 37) {
+    var e = tb[9].childNodes[0].lastElementChild.lastElementChild.value;
+  } else {
+    var e = tb[10].childNodes[0].lastElementChild.lastElementChild.value;
+  }
+  if (tb.length == 37) {
+    var f = tb[9].childNodes[2].lastElementChild.lastElementChild.value;
+  } else {
+    var f = tb[10].childNodes[2].lastElementChild.lastElementChild.value;
+  }
+  if (tb.length == 37) {
+    var g = tb[9].childNodes[4].lastElementChild.lastElementChild.value;
+  } else {
+    var g = tb[10].childNodes[4].lastElementChild.lastElementChild.value;
+  }
+  if (tb.length == 37) {
+    var h = tb[8].lastElementChild.value;
+  } else {
+    var h = tb[9].lastElementChild.value;
+  }
+
+  let ContactOK = a;
+  let PtnrContactOK = b;
+  let ContactOffers = c;
+  let ContactNewsltr = d;
+  let GoodEmail = e;
+  let GoodMobile = f;
+  let GoodAddr = g;
+  let contact = h;
 
   console.log('Username: '+username);
   console.log('Account No: '+accno);
@@ -71,8 +114,11 @@ docReady(function() {
   let pot = 'pot';
   let sendInfo = ''+accno+','+group+','+custID+','+pot+'';
 
-  if (potential !== null) {
+
+  if (potential === "red") {
+
     console.log('Found Potential Match!');
+
     browser.runtime.sendMessage(sendInfo);
   } else {
     let sendInfo2 = ''+accno+','+group+','+custID+'';
