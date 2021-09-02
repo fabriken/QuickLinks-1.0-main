acd6887166c9419d7f236a4e4f62fc04fb852ffc
fabriken <techtux@gmail.com>
2021-09-02 22:05:14 +0930

    Update sidebar-style.js
    Final version

--- sidebar-style.js
+++ sidebar-style.js
@@ -1,42 +1,112 @@
-function setSidebarStyle(theme) {
-  const myElement = document.getElementById("style");
-
-  if (theme.colors && theme.colors.frame) {
-    document.body.style.backgroundColor =
-      theme.colors.frame;
-  } else {
-    document.body.style.backgroundColor = "black";
-  }
-
-  if (theme.colors && theme.colors.toolbar) {
-    myElement.style.backgroundColor = theme.colors.toolbar;
-  } else {
-    myElement.style.backgroundColor = "white";
-  }
-
-  if (theme.colors && theme.colors.toolbar_text) {
-    myElement.style.color = theme.colors.toolbar_text;
-  } else {
-    myElement.style.color = "white";
-  }
+input:hover {
+	filter: brightness(0.9);
 }
-
-// Set the element style when the extension page loads
-async function setInitialStyle() {
-  const theme = await browser.theme.getCurrent();
-  setSidebarStyle(theme);
+.btn_class:hover {
+	filter: brightness(0.9);
 }
-setInitialStyle();
-
-// Watch for theme updates
-browser.theme.onUpdated.addListener(async ({ theme, windowId }) => {
-  const sidebarWindow = await browser.windows.getCurrent();
-  /*
-    Only update theme if it applies to the window the sidebar is in.
-    If a windowId is passed during an update, it means that the theme is applied to that specific window.
-    Otherwise, the theme is applied globally to all windows.
-  */
-  if (!windowId || windowId == sidebarWindow.id) {
-    setSidebarStyle(theme);
-  }
-});
+p {
+  user-select: all;
+}
+input[id=search] {
+	width: 100%;
+	box-sizing: border-box;
+	border: 2px solid #ccc;
+	border-radius: 6px;
+	background-color: white;
+	background-image: url('icons/Search.svg');
+	background-position: 4px 8px;
+	background-repeat: no-repeat;
+	padding: 8px 8px 8px 24px;
+}
+input[id=pickActive] {
+	background-color: #4E9B9B;
+	color: white;
+	border-radius: 6px;
+	padding: 8px 8px;
+}
+input[id=reason] {
+	background-color: white;
+	box-sizing: border-box;
+	border: 2px solid #ccc;
+	border-radius: 6px;
+	background-position: 14px 14px;
+	background-repeat: no-repeat;
+	padding: 8px 8px;
+}
+input[id=activate] {
+	background-color: #99CCCC;
+	color: white;
+	border-radius: 6px;
+	padding: 8px 8px;
+}
+input[id=suspend] {
+	background-color: #4E9B9B;
+	color: white;
+	border-radius: 6px;
+	padding: 8px 8px;
+}
+input[id=close] {
+	background-color: #99CCCC;
+	color: white;
+	border-radius: 6px;
+	padding: 8px 8px;
+}
+input[id=closeDEP] {
+	background-color: #4E9B9B;
+	color: white;
+	border-radius: 6px;
+	padding: 8px 8px;
+}
+input[id=closeDMA] {
+	background-color: #99CCCC;
+	color: white;
+	border-radius: 6px;
+	padding: 8px 8px;
+}
+input[id=pickGroup] {
+	background-color: #4E9B9B;
+	color: white;
+	border-radius: 6px;
+	padding: 8px 8px;
+}
+input[id=assign] {
+	background-color: #99CCCC;
+	color: white;
+	border-radius: 6px;
+	padding: 8px 8px;
+}
+input[id=removeGr] {
+	background-color: #4E9B9B;
+	color: white;
+	border-radius: 6px;
+	padding: 8px 8px;
+}
+input[id=clearFlags] {
+	background-color: #99CCCC;
+	color: white;
+	border-radius: 6px;
+	padding: 8px 8px;
+}
+.loader {
+	border: 5px solid #f3f3f3;
+	border-radius: 50%;
+	border-top: 5px solid gray;
+	padding: 8px 16px;
+	margin: auto;
+	width: 40px;
+	height: 40px;
+	-webkit-animation: spin 1s linear infinite;
+	animation: spin 1s linear infinite;
+}
+@-webkit-keyframes spin {
+	0% { -webkit-transform: rotate(0deg); }
+	100% { -webkit-transform: rotate(360deg); }
+}
+@keyframes spin {
+	0% { transform: rotate(0deg); }
+	100% { transform: rotate(360deg); }
+}
+p {
+	text-align: center;
+	font-size: 14px;
+}
